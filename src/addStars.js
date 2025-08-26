61328bdfdea5a4265576d90819ed59b97f2fa80b
bobby reed <37113385+bobbyreed@users.noreply.github.com>
2025-08-26 16:19:43 -0500

    Adding moon svg

--- src/addStars.js
+++ src/addStars.js
@@ -1,9 +1,10 @@
 import { Graphics } from 'pixi.js';
+import moonSVG from '../public/assets/moon.svg';
 
 export function addStarsAndMoon(app) {
   const starCount = 20;
     const stars = new Graphics();
-    const moon = new Graphics().svg('../public/assets/moon.svg');
+    const moon = new Graphics().svg(moonSVG);
 
     for (let index = 0; index < starCount; index++) {
         const x = (index * 0.78695 * app.screen.width) % app.screen.width;
